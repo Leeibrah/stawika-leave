ad65521b8936e46bab50e4cffa1eff397b053f52
Lee Ibrahim <leeibrah@gmail.com>
2026-03-19 16:04:43 +0300

    Update smtp.php
    Remove zoho password

--- src/smtp.php
+++ src/smtp.php
@@ -6,6 +6,6 @@ return [
         'encryption' => 'tls',
         'from' => 'no-reply@stawika.co.ke', // must be a valid Zoho email
         'username' => 'no-reply@stawika.co.ke',
-        'password' => 'WnvmzpZzGXbU',
+        'password' => '',
     ]
 ];
